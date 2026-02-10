0eb7aeb940fa0507ccde55cb377d5b76a59743a1
ge2009 <ge2009@gmail.com>
2026-02-10 10:26:01 +0800

    Add Posthog tracking to blog

--- src/components/analytics/PostHogProvider.tsx
+++ src/components/analytics/PostHogProvider.tsx
@@ -1,27 +1,20 @@
 'use client'
 
 import { useEffect } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from '@posthog/react'
 
 function PostHogPageView() {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
 
   useEffect(() => {
     if (!pathname || !process.env.NEXT_PUBLIC_POSTHOG_KEY) return
 
-    let url = window.origin + pathname
-    const qs = searchParams?.toString()
-    if (qs) {
-      url = `${url}?${qs}`
-    }
-
     posthog.capture('$pageview', {
-      $current_url: url,
+      $current_url: window.location.href,
     })
-  }, [pathname, searchParams])
+  }, [pathname])
 
   return null
 }
@@ -52,4 +45,3 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     </PHProvider>
   )
 }
-
